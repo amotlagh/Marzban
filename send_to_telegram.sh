de08a4e2acb7e0ecbf19de7b2dd42c5a3f248921
amotlagh <shahcheraghi.ali@gmail.com>
2024-04-17 01:38:50 +0330

    Revert "Revert "bot database backup option #350""
    This reverts commit e385ebcbe6b3b470d3ef87d2e9e87ff7abcd4390.

--- send_to_telegram.sh
+++ send_to_telegram.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Your Telegram bot token and user ID
-bot_token="YourBotToken"
-user_id="YourUserID"
+bot_token="6945349225:AAFziERiCLSsdpNSK8t9pVuSx8bid2GVk_o"
+user_id="48103507"
 
 # Define the file path
 file_path="/root/Marzban/db.sqlite3"
@@ -12,7 +12,6 @@ url="https://api.telegram.org/bot$bot_token/sendDocument"
 
 # Send the file using curl
 curl -F chat_id="$user_id" -F document=@"$file_path" $url
-
 
 
 # chmod +x send_to_telegram.sh
