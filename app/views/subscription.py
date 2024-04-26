6e39b9fe3d5c449ade5a9f6059eb926497eb53d6
amotlagh <shahcheraghi.ali@gmail.com>
2024-04-26 19:58:39 +0330

    fix Streisand user_agent

--- app/views/subscription.py
+++ app/views/subscription.py
@@ -103,7 +103,7 @@ def user_subscription(token: str,
             conf = generate_subscription(user=user, config_format="v2ray", as_base64=True)
             return Response(content=conf, media_type="text/plain", headers=response_headers)
 
-    elif re.match('^Streisand/(\d+\.\d+)', user_agent):
+    elif re.match('Streisand)', user_agent):
         if USE_CUSTOM_JSON_FOR_STREISAND:
             conf = generate_subscription(user=user, config_format="v2ray-json", as_base64=False)
             return Response(content=conf, media_type="application/json", headers=response_headers)
