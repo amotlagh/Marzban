fe1f68b1a61f7c875fab5064397733ec633e7c9c
amotlagh <shahcheraghi.ali@gmail.com>
2024-05-28 21:05:18 +0330

    fix: mux settings  /commit/16ed7ec1bcd73a08248781f30d733685b8f7970a

--- app/subscription/singbox.py
+++ app/subscription/singbox.py
@@ -148,7 +148,7 @@ class SingBoxConfiguration(str):
             "server": address,
             "server_port": port,
         }
-        if net in ('tcp', 'kcp') and headers != 'http' and tls != 'none':
+        if net in ('tcp', 'kcp') and headers != 'http' and (tls or tls != 'none'):
             if flow:
                 config["flow"] = flow
 
@@ -183,13 +183,11 @@ class SingBoxConfiguration(str):
                                             pbk=pbk, sid=sid, alpn=alpn,
                                             ais=ais)
 
-        # Singbox mux is not compatible with xray mux
-        # Since marzban doesnt support singbox core at server side at the moment - sinbox-mux should not be enabled at all
-        # if mux_enable:
-        #     mux_json = json.loads(self.mux_template)
-        #     mux_config = mux_json["sing-box"]
-        #     config['multiplex'] = mux_config
-        #     config['multiplex']["enabled"] = mux_enable
+        if mux_enable:
+            mux_json = json.loads(self.mux_template)
+            mux_config = mux_json["sing-box"]
+            config['multiplex'] = mux_config
+            config['multiplex']["enabled"] = mux_enable
 
         return config
 
